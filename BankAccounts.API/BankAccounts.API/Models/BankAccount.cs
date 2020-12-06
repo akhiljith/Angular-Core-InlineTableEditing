35aedc5f10f54dd6fb94be5ecbb29ef70bb045f4
akhiljith <akhiljith.c.a@gmail.com>
2020-12-06 15:49:51 +0530

    Created Asp.Net Core project and required Model classes

---
+++ BankAccounts.API/BankAccounts.API/Models/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAccounts.API.Models
+{
+    public class BankAccount
+    {
+        public int BankAccountID { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(20)")]
+        public string AccountNumber { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string AccountHolder { get; set; }
+        [Required]
+        public int BankID { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string iFSC { get; set; }
+    }
+}
